4130f76278b50039aa0808f85ee2b984900311b7
isaias005 <isaias005@hotmail.com>
2020-03-05 22:42:16 -0400

    modified dependencies

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const port = process.env.PORT || 5000;
 
@@ -12,6 +19,26 @@ const auth = require('./routes/auth');
 const users = require('./routes/users');
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(rateLimit({windowMs: 10 * 60 * 1000, max:10000}));
+app.use(hpp());
+
+const whitelist =["http://localhost:3000","https://bootcamper-5a096.web.app"];
+app.use(cors({
+    credentials:true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    origin: function(origin, callback){
+        if(whitelist.indexOf(origin) !== -1){
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by cors"));
+        }
+    }
+}));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
